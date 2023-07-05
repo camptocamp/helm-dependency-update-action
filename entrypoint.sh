26ac098a3629992745ef1635112420d4aea1054f
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-07-05 09:18:29 +0200

    fix: correct error correction condition in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,11 @@ dry_run=$( [ $1 == "true" ] && echo "--dry-run" || echo "" )
 python3 /helm_dependency_bumper.py $dry_run --chart $2 --upgrade-strategy $3 --exclude-dependency $4
 
 # Exit with error code if the Python script fails
-if [ $(echo $?) ]; then
+if [ $(echo $?) == 1 ]; then
   exit 1
 fi
+
+rm manifest.yaml
 
 # Return a boolean indicating whether the any dependency was upgraded
 upgraded=$( [[ `git status --porcelain` ]] && echo true || echo false )
