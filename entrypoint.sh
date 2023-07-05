9db06d082875471a70e822a50d7184473230e315
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-07-05 09:53:15 +0200

    fix: fix git issue with file ownership inside the workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Add the "--dry-run" flag if the dry-run argument is "true"
 dry_run=$( [ $1 == "true" ] && echo "--dry-run" || echo "" )
 
+# Run the Python script
 python3 /helm_dependency_bumper.py $dry_run --chart $2 --upgrade-strategy $3 --exclude-dependency $4
 
 # Exit with error code if the Python script fails
@@ -10,7 +11,11 @@ if [ $(echo $?) == 1 ]; then
   exit 1
 fi
 
+# Remove the manifest so it is not committed to the repository
 rm manifest.yaml
+
+# Set up the workspace directory as a safe directory for Git otherwise the git command will fail
+git config --global --add safe.directory /github/workspace
 
 # Return a boolean indicating whether the any dependency was upgraded
 upgraded=$( [[ `git status --porcelain` ]] && echo true || echo false )
