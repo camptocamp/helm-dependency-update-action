7f1dea5cb8cbe21d2a8331b799d58ba36ea99e30
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-07-14 13:01:23 +0200

    fix: add management of empty exclusion variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Add the "--dry-run" flag if the dry-run argument is "true".
-dry_run=$( [ $1 == "true" ] && echo "--dry-run" || echo "" )
+# Add the "--exclude-dependency" flag if the exclude-dependency argument is not empty.
+exclude_dependency=$( [ $3 != "" ] && echo "--exclude-dependency $3" || echo "" )
 
 # Add the "--update-readme" flag if the update-readme argument is not empty.
-update_readme=$( [ $2 != "" ] && echo "--update-readme $2" || echo "" )
+update_readme=$( [ $4 != "" ] && echo "--update-readme $4" || echo "" )
+
+# Add the "--dry-run" flag if the dry-run argument is "true".
+dry_run=$( [ $5 == "true" ] && echo "--dry-run" || echo "" )
 
 # Run the Python script.
-python3 /helm_dependency_bumper.py $dry_run \
+python3 /helm_dependency_bumper.py --chart $1 \
+                                   --upgrade-strategy $2 \
+                                   $exclude_dependency \
+                                   $dry_run \
                                    $update_readme \
-                                   --chart $3 \
-                                   --upgrade-strategy $4 \
-                                   --exclude-dependency $5 \
                                    --output output.txt
 
 # Exit with error code if the Python script fails.
